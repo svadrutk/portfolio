f52ea8b75228aa6ecb6c764b1d1a2b0bc9c5dd32
Swad Kukunooru <kukunoorusvadrut@gmail.com>
2026-02-02 18:52:57 -0500

    new resume

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1762125300953,
+    __BUILD_TIMESTAMP_MS__: 1766354437164,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
